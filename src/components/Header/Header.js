d135d6dac22403dbb79192493dd206bd5c8d9303
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.lan>
2023-08-05 14:36:52 -0500

    responsive nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,8 +10,8 @@ function Header() {
 
     return (
         <header className="Header">
-            <div className="top-content">
-                <img className="mobile-logo" src="./images/Home/Mobile_Kirchhayn_Logo.png" alt=""/>
+            <div className="mobile-top-content">
+                <img className="mobile-logo padding-inline" src="./images/Home/Mobile_Kirchhayn_Logo.png" alt=""/>
                 <div className="right-content">
                     <img className="phone-icon" src="./images/Home/phone-icon.png" alt=""/>
                     <button className={`nav_toggle ${openNav ? 'hamburger-cooked' : ''}`} onClick={handleNavToggle}>
@@ -20,11 +20,13 @@ function Header() {
                 </div>
             </div>
 
+            <img className="desktop-logo hover-animation" src="./images/Home/logo.png" alt=""/>
+
             <nav className={`nav ${openNav ? 'open-nav' : ''}`}>
                 <ul className="nav-list">
-                    <li className="nav-item">Search Used Car Parts</li> 
-                    <li className="nav-item">Search Used Cars</li>
-                    <li className="nav-item">Sell Your Car</li>
+                    <li className="nav-item"><a>Search Used Car Parts</a></li> 
+                    <li className="nav-item"><a>Search Used Cars</a></li>
+                    <li className="nav-item"><a>Sell Your Car</a></li>
                     <li className="has-dropdown nav-item">
                         <a>Resources</a>
                         <input type="checkbox" id="submenu-checkbox"/>
@@ -33,13 +35,21 @@ function Header() {
                         </label>
 
                         <ul className="dropdown">
-                            <li>Category1</li>
-                            <li>Category2</li>
-                            <li>Category3</li>
+                            <li><a>Category1</a></li>
+                            <li><a>Category2</a></li>
+                            <li><a>Category3</a></li>
                         </ul>
                     </li>
-                    <li className="nav-item">Contact Us</li>
+                    <li className="nav-item move-up"><a>Contact Us</a></li>
                 </ul>
+                <a className="phone-info hover-animation">
+                    <img src="./images/Home/phone-icon.png" alt=""/>
+
+                    <div className="text">
+                        <p>Call Us Today!</p>
+                        <span>555 555 5555</span>
+                    </div> 
+                </a>
             </nav>
         </header>
     );
