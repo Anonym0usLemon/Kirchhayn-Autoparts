18e36afc5639c93fa7bea5a054890743b3905250
Dominic DiModugno <anonym0uslemon@Dominics-Laptop.lan>
2023-08-22 10:14:22 -0500

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,42 +3,50 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
         <footer>
-            <div className="logo-container">
-                <img className="hover-animation" src="./images/Home/logo.png" alt=""/>
-            </div>
-            <div className="contact-container">
-                <p className="title">Contact Us</p>
-                <div className="info-container">
-                    <img src="./images/Home/yellow-phone-icon.png" alt=""/>
-                    <div className="info">
-                        <p className="number">555 555 5555</p>
-                        <p className="align-end">Mon - Fri: 8:00 - 5:00 <br/>Sat: 9:00 - 1:00</p>
+            <div className="desktop-container">
+                <div className="logo-container">
+                    <img className="hover-animation" src="./images/Home/logo.png" alt=""/>
+                </div>
+                <div className="desktop-wrapper">
+                    <div className="desktop-info">
+                        <div className="contact-container">
+                            <p className="title">Contact Us</p>
+                            <div className="info-container">
+                                <img src="./images/Home/yellow-phone-icon.png" alt=""/>
+                                <div className="info">
+                                    <p className="number">555 555 5555</p>
+                                    <p className="align-end">Mon - Fri: 8:00 - 5:00 <br/>Sat: 9:00 - 1:00</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="address-container">
+                            <img src="./images/Home/yellow-pin.png" alt=""/>
+                            <div className="info">
+                                <p className="title">Address</p>
+                                <p className="align-end">W4726 County Road A <br/> Fredonia, WI 53021</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="our-services">
+                        <p>Our Services</p>
+                        <Link>Search Used Car Parts</Link>
+                        <Link>Search Used Cars</Link>
+                        <Link>Sell Your Car</Link>
+                        <Link>Contact Us</Link>
+                    </div>
+                    <div className="resources">
+                        <p>Resouces</p>
+                        <Link>Cut Sheets</Link>
+                        <Link>Warranty</Link>
+                        <Link>Testimonials</Link>
+                        <Link className="yellow">Donate</Link>
                     </div>
                 </div>
             </div>
-            <div className="address-container">
-                <img src="./images/Home/yellow-pin.png" alt=""/>
-                <div className="info">
-                    <p className="title">Address</p>
-                    <p className="align-end">W4726 County Road A <br/> Fredonia, WI 53021</p>
-                </div>
+            <div className="desktop-cards">
+                <img className="credit-cards" src="./images/Home/Credit-Cards.png" alt="" />
+                <p className="credits">Developed By - Dominic DiModugno</p>
             </div>
-            <div className="our-services">
-                <p>Our Services</p>
-                <Link>Search Used Car Parts</Link>
-                <Link>Search Used Cars</Link>
-                <Link>Sell Your Car</Link>
-                <Link>Contact Us</Link>
-            </div>
-            <div className="resources">
-                <p>Resouces</p>
-                <Link>Cut Sheets</Link>
-                <Link>Warranty</Link>
-                <Link>Testimonials</Link>
-                <Link className="yellow">Donate</Link>
-            </div>
-            <img className="credit-cards" src="./images/Home/Credit-Cards.png" alt="" />
-            <p className="credits">Developed By - Dominic DiModugno</p>
         </footer>
     );
 }
